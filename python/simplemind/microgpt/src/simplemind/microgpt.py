d519e1ba98004313c07cfd390e927180479c2b24
Michael Bosworth <michael.a.bosworth@gmail.com>
2026-03-30 23:28:27 +0200

    add some slow tests

--- python/simplemind/microgpt/src/simplemind/microgpt.py
+++ python/simplemind/microgpt/src/simplemind/microgpt.py
@@ -1,2 +1,6 @@
-def main() -> None:
-    print("Hello from simplemind-microgpt!")
+from typing import Callable
+from karpathy.microgpt import main as kmain
+
+
+def main(num_training_steps=1000, emit: Callable[[str], None] = lambda emission: print(emission)) -> None:
+    kmain(num_training_steps, emit)
--- python/simplemind/microgpt/tests/microgpt_test.py
+++ python/simplemind/microgpt/tests/microgpt_test.py
@@ -1,2 +1,29 @@
-def test_microgpt() -> None:
-    assert True
+from pathlib import Path
+from typing import Callable
+
+from karpathy.microgpt import main as kmain
+from simplemind.microgpt import main as smain
+
+
+def emitter(vals: list[str]) -> Callable[[str], None]:
+    def emit(val: str) -> None:
+        vals.append(val)
+
+    return emit
+
+
+def test__microgpt__output_matches_reference_output() -> None:
+    vals_s: list[str] = []
+    vals_k: list[str] = []
+    smain(25, emitter(vals_s))
+    kmain(25, emitter(vals_k))
+    assert vals_s == vals_k
+
+
+def test__precondition__reference_output_matches_known_reference_output() -> None:
+    vals_k: list[str] = []
+    kmain(100, emitter(vals_k))
+    vals_ref = [s.strip() for s in (Path(__file__).parent / "ref_100_steps.txt").read_text().splitlines()]
+    norm_vals_k = [k for k in [k.strip() for k in vals_k] if k]
+    norm_vals_ref = [r for r in [r.strip() for r in vals_ref] if r]
+    assert norm_vals_k == norm_vals_ref
